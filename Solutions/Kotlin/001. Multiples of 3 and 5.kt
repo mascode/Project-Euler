0668604f069bcf2492923dd0cba66a29322c598c
mascode <me@masouds.com>
2021-11-15 02:24:15 -0800

    Multiples of 3 and 5
    First Kotlin Solution

---
+++ Solutions/Kotlin/001. Multiples of 3 and 5.kt
@@ -0,0 +1,17 @@
+
+//Find the sum of all the multiples of 3 or 5 below 1000.
+
+fun main() {
+    var sum = 0
+    for (i in 1..999) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i
+        }
+    }
+    println(sum)
+}
+
+
+
+
+
